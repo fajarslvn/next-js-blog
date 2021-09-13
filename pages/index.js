4c72ce106527421ee506bf4e592d736851da0c4d
fajarslvn <jarphish@gmail.com>
2021-09-13 20:35:44 +0700

    init

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -9,10 +10,12 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi my name is <b>fsec</b>. I'm a freelancce Front end engineer.</p>
+        <p>Hi my name is <b>fsec</b>. I'm a freelance Backend engineer.</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        To check our first post, please{' '}
+          <Link href='/posts/first-post'>
+            <a>click here...</a>
+          </Link>
         </p>
       </section>
     </Layout>
